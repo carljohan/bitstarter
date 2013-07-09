55e0753ac558bf24e43222e44978189423e44e4f
Carl Johan Sundqvist <carljohan.sundqvist@gmail.com>
2013-07-09 20:28:00 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -2,12 +2,13 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    var fs = require('fs');
-    var content = fs.readFileSync('index.html', 'utf8');
-    response.send(buf.toString());
-});
+var fs = require('fs');
+var buffer = new buffer ();
+  response.send(buffer.toString('utc 8', fs.readFileSync("index.html")))});
+ });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+console.log("Listening on " + port);
 });
+
